74a988b0cea55176c8067493395dd1b1bfdd8180
Fellipe Caetano <fellipe.caetano4@gmail.com>
2016-12-02 18:36:14 -0200

    Consolidate workshop bootstrap

--- app/Sources/AppDelegate.swift
+++ app/Sources/AppDelegate.swift
@@ -4,14 +4,13 @@ import Redux
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    let store = Store(initialState: AppState(), reducer: AppReducer)
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.backgroundColor = .white
         self.window?.rootViewController = UINavigationController(
-            rootViewController: SubredditsViewController(store: store)
+            rootViewController: SubredditsViewController()
         )
         self.window?.makeKeyAndVisible()
         return true
--- app/Sources/ViewControllers/SubredditsViewController.swift
+++ app/Sources/ViewControllers/SubredditsViewController.swift
@@ -3,15 +3,9 @@ import Redux
 
 class SubredditsViewController: UITableViewController {
     private let cellReuseIdentifier = "SubredditTableViewCell"
+    private var subreddits = ["apple", "swift", "ios", "iphone"]
 
-    private unowned let store: Store<AppState>
-    private var unsubscribe: (() -> Void)?
-
-    private var subreddits = [String]()
-    private var selectedSubreddit: String?
-
-    init(store: Store<AppState>) {
-        self.store = store
+    init() {
         super.init(nibName: nil, bundle: nil)
         navigationItem.title = "Subreddits"
     }
@@ -27,39 +21,6 @@ class SubredditsViewController: UITableViewController {
         tableView.tableFooterView = UIView()
     }
 
-    override func viewDidLoad() {
-        store.dispatch(FetchSubreddits())
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        unsubscribe = store.subscribe { state in
-            self.render(subreddits: state.subreddits, selectedSubreddit: state.selectedSubreddit)
-        }
-    }
-
-    override func viewDidDisappear(_ animated: Bool) {
-        unsubscribe?()
-    }
-
-    private func render(subreddits: [String], selectedSubreddit: String?) {
-        receiveSubreddits(subreddits)
-        selectSubreddit(selectedSubreddit)
-    }
-
-    private func receiveSubreddits(_ subreddits: [String]) {
-        self.subreddits = subreddits
-        tableView.reloadData()
-    }
-
-    private func selectSubreddit(_ selectedSubreddit: String?) {
-        if let selectedSubreddit = selectedSubreddit, self.selectedSubreddit != selectedSubreddit {
-            let posts = PostsViewController(subreddit: selectedSubreddit, store: store)
-            navigationController?.pushViewController(posts, animated: true)
-        }
-
-        self.selectedSubreddit = selectedSubreddit
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return subreddits.count
     }
@@ -68,10 +29,5 @@ class SubredditsViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
         cell.textLabel?.text = subreddits[indexPath.row]
         return cell
-    }
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedSubreddit = subreddits[indexPath.row]
-        store.dispatch(AppAction.selectSubreddit(selectedSubreddit))
     }
 }
